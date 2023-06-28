1cbb2a7c6b92cfde185acc0a9108ed76c0252560
Lucas Schroeder <lucasschroederkm@gmail.com>
2023-06-28 11:10:54 -0300

    Create template and view login

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from .views import views
 
 urlpatterns = [
-    path('', ),
+    path('login', views.login_view, name='login'),
 ]
--- users/views/views.py
+++ users/views/views.py
@@ -5,3 +5,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        post = request.POST
+        email = post.get('email')
+        password = post.get('password')
+
+        find_user = User.objects.filter(email=email).first()
+        username = find_user.username if find_user else None
+
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            # TODO colocar o redirecionamento para a pagina home após o login
+            login(request, user)
+            return HttpResponse("Autenticado")
+        
+        # TODO colocar um retorno para a mesma pagina para dar um retorno para o cliente
+        return HttpResponse("E-mail ou senha inválidos")
+
+    return render(request, 'users/login.html')
