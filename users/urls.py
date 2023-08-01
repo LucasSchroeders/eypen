b608c6a3f03cb080c13cf2cb4b5c0fc0d4e8fd4f
Lucas Schroeder <lucasschroederkm@gmail.com>
2023-08-01 17:24:09 -0300

    Reorganizado urls login, cadastro e logout para eypen/urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,13 +3,8 @@ from users import views as views
 from company import views as views_company
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('cadastro', views.signup, name='signup'),
-    path('login', views.login_view, name='login'),
-    path('logout', auth_views.LogoutView.as_view, name="logout"),
-    path('cadastro/2', views.signup2, name='signup2'),
     path('profile/<int:id>/edit', views.PersonalProfileInformation.as_view(), name='profile_edit'),
     path('profile/<int:id>', views.ProfileApplicant.as_view(), name='profile'),
     path('profile/busca-perfil', views.BuscaPerfil.as_view(), name='busca_perfil'),
