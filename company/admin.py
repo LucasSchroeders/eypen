0be3c44d3662b0f55ee22be919cf05d266c45b70
Lucas Schroeder <lucasschroederkm@gmail.com>
2023-08-14 11:09:37 -0300

    Exibicao de company no admin-django

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Company
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'cnpj',
+    )
+    search_fields = ('name', 'cnpj')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request)
