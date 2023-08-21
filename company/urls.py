d9d987fd389a9f6919d29aceb90eacd106928580
Lucas Schroeder <lucasschroederkm@gmail.com>
2023-08-21 14:02:37 -0300

    Ajuste de view na url do processo seletivo

--- company/urls.py
+++ company/urls.py
@@ -4,7 +4,5 @@ from company import views as views
 urlpatterns = [
     path('company/<int:id>', views.CompanyProfile.as_view(), name='company_profile'),
     path('company/<int:id>/edit', views.companyRegister, name='company_edit'),
-    path('company/vagas', views.VacancyTemplateView.as_view(), name='exibir_vagas'), #TODO ao fazer o back, colocar os ids das vaga e empresa
-    path('company/vagas/2', views.VacancyRegisterTemplateView.as_view(), name='registrar_vagas'), #TODO ao fazer o back, colocar os ids das vaga e empresa
-    path('company/vagas/selective-process', views.VacancyRegisterTemplateView.as_view(), name='vagas_processo_seletivo'), #TODO ao fazer o back, colocar os ids das vaga e empresa
+    path('company/vagas/selective-process', views.VacancySelectiveProcessTemplateView.as_view(), name='vagas_processo_seletivo'), #TODO ao fazer o back, colocar os ids das vaga e empresa
 ]
