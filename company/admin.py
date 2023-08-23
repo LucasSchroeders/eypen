6e05b11aa46ab3a16d092f02b4c80d2ba226e5b2
Lucas Schroeder <lucasschroederkm@gmail.com>
2023-08-23 10:58:52 -0300

    Adicionado os modelos de vagas e etapas no admin django

--- company/admin.py
+++ company/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Company
+from .models import Company, Vacancy, Step
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
@@ -10,6 +10,33 @@ class CompanyAdmin(admin.ModelAdmin):
         'cnpj',
     )
     search_fields = ('name', 'cnpj')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request)
+
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'job_position',
+    )
+    search_fields = ('job_position', 'company__name')
+    readonly_fields = ('company',)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request)
+
+
+@admin.register(Step)
+class StepAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'step',
+        'title',
+    )
+    search_fields = ('title', 'vacancy__company__name')
+    readonly_fields = ('vacancy',)
 
     def get_queryset(self, request):
         return super().get_queryset(request)
