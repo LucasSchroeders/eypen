5830e8d0593c66c46be12e598572a8f109354a72
Lucas Schroeder <lucasschroederkm@gmail.com>
2023-08-28 16:17:13 -0300

    Adicionado url da API StepAPIView

--- company/urls.py
+++ company/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('company/<int:id>/vagas/cadastro', views.create_vacancy, name='registrar_vagas'),
     path('company/<int:id>/vagas/<int:id_vacancy>/edit', views.update_vacancy, name='editar_vagas'),
     path('company/<int:id>/vagas/<int:id_vacancy>/step', views.StepTemplateView.as_view(), name='registrar_etapa'),
+    path('api/vagas/<int:id>/step', views.StepAPIView.as_view(), name='save_step'),
     path('company/vagas/selective-process', views.VacancySelectiveProcessTemplateView.as_view(), name='vagas_processo_seletivo'), #TODO ao fazer o back, colocar os ids das vaga e empresa
     path('company/busca-vagas', views.BuscaVacancy.as_view(), name='busca_vagas'),
 ]
--- company/views/__init__.py
+++ company/views/__init__.py
@@ -1,3 +1,3 @@
 from .company import BuscaCompany, CompanyProfile, companyRegister
-from .step import StepTemplateView
+from .step import StepTemplateView, StepAPIView
 from .vacancy import create_vacancy, update_vacancy, BuscaVacancy, VacancyTemplateView, VacancySelectiveProcessTemplateView
