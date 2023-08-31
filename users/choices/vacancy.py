13918dd27a1646260c666780f9d05b1604d52657
Lucas Schroeder <lucasschroederkm@gmail.com>
2023-08-31 13:58:08 -0300

    Criado novo choices para vacancy (status_vacancy_choices) e ajuste em vulnerabilities_choices

--- users/choices/vacancy.py
+++ users/choices/vacancy.py
@@ -8,18 +8,25 @@ SENIORITY_CHOICES = [
 ]
 
 VULNERABILITIES_CHOICES = [
-    ('masculino', 'Masculino'),
-    ('feminino', 'Feminino'),
-    ('neutro', 'Neutro'),
-    ('nao_binario', 'Não binário'),
-    ('transgenero', 'Transgênero'),
-    ('auditiva', 'Auditiva'),
-    ('visual', 'Visual'),
-    ('motora', 'Motora'),
-    ('mental', 'Metal'),
+    ('todos', 'Todos'),
+    ('homens', 'Homens'),
+    ('mulheres', 'Mulheres'),
+    ('neutro', 'Gênero neutro'),
+    ('nao_binario', 'Gênero Não binário'),
+    ('transgenero', 'Gênero Transgênero'),
+    ('auditiva', 'Pessoas com deficiência auditiva'),
+    ('visual', 'Pessoas com deficiência visual'),
+    ('motora', 'Pessoas com deficiência motora'),
+    ('mental', 'Pessoas com deficiência mental'),
     ('branco', 'Branco'),
     ('preto', 'Preto'),
     ('amarelo', 'Amarelo'),
     ('indigena', 'Indígena'),
     ('pardo', 'Pardo'),
+]
+
+STATUS_VACANCY_CHOICES = [
+    ('ATV', 'Ativo'),
+    ('PEN', 'Pendente'),
+    ('ENC', 'Encerrado'),
 ]
