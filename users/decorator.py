ede370033e4f6b3a956c72b601ca4b14c1ab3a78
Lucas Schroeder <lucasschroederkm@gmail.com>
2023-08-31 16:04:23 -0300

    Criado novo decorator para validar se a vaga possui os status para visualizar

--- users/decorator.py
+++ users/decorator.py
@@ -5,6 +5,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.urls import reverse
 
+from company.models import Vacancy
+
 
 def company_only(function):
     def _inner(request, *args, **kwargs):
@@ -55,6 +57,34 @@ def applicant_only(function):
             )
             return redirect('company_profile', id=profile.company.id)
 
+        return function(request, *args, **kwargs)
+    
+    return _inner
+
+
+def vacancy_status_invalid(function):
+    def _inner(request, *args, **kwargs):
+        vacancy = Vacancy.objects.filter(id=kwargs.get('id_vacancy')).first()
+        profile = request.user.profile
+
+        if vacancy.status in ('INTV', 'ENC') and profile.is_applicant:
+            messages.add_message(
+                request,
+                messages.ERROR,
+                'A vaga que você está tentando acessar está inativa ou o processo seletivo já foi finalizado!',
+                extra_tags='Vaga',
+            )
+            return redirect('busca_vagas')
+        
+        elif profile.is_company and not profile.company.id == vacancy.company.id:
+            messages.add_message(
+                request,
+                messages.ERROR,
+                'Você não tem acesso a esse conteúdo! A vaga que você está tentando acessar não é da sua empresa!',
+                extra_tags='Vaga',
+            )
+            return redirect('company_profile', id=profile.company.id)
+        
         return function(request, *args, **kwargs)
     
     return _inner
