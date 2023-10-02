40b473f0fb3de3af5b495e432d9f54cfe873af6b
Lucas Schroeder <lucasschroederkm@gmail.com>
2023-10-02 16:16:01 -0300

    Criando novos filtros e exibindo genero, etnia e deficiencia

--- users/templatetags/custom_filter.py
+++ users/templatetags/custom_filter.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from django import template
 
 from users.choices import (
@@ -6,7 +6,10 @@ from users.choices import (
     SENIORITY_CHOICES,
     JOB_MODALITY,
     JOB_TYPE,
-    VULNERABILITIES_CHOICES
+    VULNERABILITIES_CHOICES,
+    GENDER_CHOICES,
+    ETHNICITY_CHOICES,
+    DISABLED_CHOICES,
 )
 
 register = template.Library()
@@ -48,6 +51,27 @@ def display_vulnerabilities(value):
 
 
 @register.filter
+def display_gender(value):
+    for k, v in GENDER_CHOICES:
+        if k == value:
+            return v
+
+
+@register.filter
+def display_disabled(value):
+    for k, v in DISABLED_CHOICES:
+        if k == value:
+            return v
+
+
+@register.filter
+def display_ethnicity(value):
+    for k, v in ETHNICITY_CHOICES:
+        if k == value:
+            return v
+
+
+@register.filter
 def filter_step(value, attr):
     result = value.filter(**eval(attr)).first()
     return result.step
@@ -58,3 +82,9 @@ def filter_notifications(value):
     yesterday = datetime.now() - timedelta(days=1)
     result = value.filter(created_at__gte=yesterday).order_by('created_at')
     return result
+
+
+@register.filter
+def filter_age(value):
+    today = date.today() 
+    return today.year - value.year - ((today.month, today.day) < (value.month, value.day)) 
